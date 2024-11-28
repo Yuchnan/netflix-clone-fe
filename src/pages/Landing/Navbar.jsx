39546db50dcbd3897aa1e7bd39ef689296a9c902
Ramandhanu Yuchnan <116786721+Yuchnan@users.noreply.github.com>
2024-11-28 12:42:11 +0700

    Update Navbar.jsx

--- src/pages/Landing/Navbar.jsx
+++ src/pages/Landing/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
                 </div>
                 <div className="flex flex-wrap items-center gap-4">
                     <OptionLanguage/>
-                    <DefaultButton text={"Sign In"} onClick={() => navigate("/browse")} />
+                    <DefaultButton text={"Browse Movies"} onClick={() => navigate("/browse")} />
                 </div>
             </nav>
         </header>
